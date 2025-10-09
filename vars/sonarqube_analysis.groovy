cd9e0fdab009924efd4346c51990e05ccb91aadd
Arun Rao <iarunrao1.in@gmail.com>
2025-10-09 14:02:09 +0530

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,14 +1,18 @@
 def call(String SonarQubeAPI, String Projectname, String ProjectKey) {
-    // this defines the Sonar Scanner path inside the function itself
+    // Jenkins में configure किया गया SonarQube Scanner tool
     def scannerHome = tool 'Sonar'
 
+    // Jenkins में "Manage Jenkins → Configure System → SonarQube servers" में जो नाम दिया है वही यहाँ पास करें
     withSonarQubeEnv("${SonarQubeAPI}") {
         sh """
             ${scannerHome}/bin/sonar-scanner \
             -Dsonar.projectName=${Projectname} \
             -Dsonar.projectKey=${ProjectKey} \
             -Dsonar.sources=. \
+            -Dsonar.host.url=${SONAR_HOST_URL} \
+            -Dsonar.login=${SONAR_AUTH_TOKEN} \
             -X
         """
     }
 }
+
